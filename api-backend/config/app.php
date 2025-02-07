dc0535e6a28ceebb9c710892cff1a1a1740f3f3d
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2025-02-07 14:46:34 +0600

    Connected to database and connect backend with frontend

--- api-backend/config/app.php
+++ api-backend/config/app.php
@@ -56,6 +56,10 @@ return [
 
     'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
     'asset_url' => env('ASSET_URL', null),
 
     /*
