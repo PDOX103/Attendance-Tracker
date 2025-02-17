0b44499e9a64dc3ec7306ba8565821f08bd86855
Priyom-17 <priyomparial17@gmail.com>
2025-02-18 02:54:07 +0600

    Implemented role-based login for Google Sign-In

---
+++ frontend/src/Pages/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div className="container mx-auto text-center">
+      <h1 className="text-2xl font-bold">AdminPage!</h1>
+    </div>
+  );
+};
+
+export default AdminPage;
---
+++ frontend/src/Pages/InstructorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InstructorPage = () => {
+  return (
+    <div className="container mx-auto text-center">
+      <h1 className="text-2xl font-bold">InstructorPage!</h1>
+    </div>
+  );
+};
+
+export default InstructorPage;
